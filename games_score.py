738a3a9693efa7c53162a8768bc53d60fe9cd9b5
daniel <122309711+Dovafighter@users.noreply.github.com>
2024-04-06 00:48:35 +0200

    Trying to fix the list

--- games_score.py
+++ games_score.py
@@ -1,9 +1,12 @@
 from psn_games import get_games
+import re
 
 def create_links(games):
     links = []
     for game in games:
-        game = game.lower().replace(' ', '-').replace("'", "")
+        game = game.replace('PS4', '').replace('PS5','').replace("&","").lower()
+        game = re.sub('(   )|(®)|', '', game)
+        game = re.sub('(®: )|(: )|( )','-',game)
         link = f"https://www.metacritic.com/game/{game}/"
         links.append(link)
 
