514332dad22eb4dd56a423b5fb6a154e8313d2fd
daniel <122309711+Dovafighter@users.noreply.github.com>
2024-04-05 14:10:19 +0200

    Created a function for getting the list of games in PS Plus Extra

---
+++ psn_games.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_games():   
+    #Function for getting a list of games in PS Plus Extra
+    URL = 'https://www.playstation.com/pl-pl/ps-plus/games/'
+    page = requests.get(URL)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    games_html = soup.find_all('a', attrs={'module-name': 'PS Plus Games List'})
+
+    games_list = [game.text for game in games_html]
+    return(games_list)
+
+games = get_games()
+print(games)
